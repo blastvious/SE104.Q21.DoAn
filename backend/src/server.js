41ec379c7982e7a1d783fdb9f6d11bce0c5690ca
blastvious <silverhd280105@gmail.com>
2026-04-28 20:54:25 +0700

    fixed server

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,6 @@ import { connectDB } from '../libs/db.js';
 import studentRoute from '../routes/student.route.js'
 import classRoute from "../routes/class.router.js"
 import authRoute from '../routes/auth.route.js'
-import classRoute from '../routes/class.router.js'
 import helmet from 'helmet'
 
 import cors from 'cors';
@@ -24,7 +23,6 @@ app.use("/api/school", authRoute);
 app.use("/api/school", studentRoute);
 app.use("/api/school", classRoute);
 
-app.use('/api/school', classRoute);
 
 app.listen(PORT, async () => {
     console.log(`Server is running at http://localhost:${PORT}`);
