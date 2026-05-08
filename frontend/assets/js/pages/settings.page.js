061edbeb21b39975ef71502441e2eb7b07db9586
blastvious <silverhd280105@gmail.com>
2026-05-08 22:04:10 +0700

    Update UI

--- frontend/assets/js/pages/settings.page.js
+++ frontend/assets/js/pages/settings.page.js
@@ -107,7 +107,8 @@ async function loadAllData() {
 
     // Render Bảng Lớp
     document.querySelector('#table-class tbody').innerHTML = classes.map(c => `
-        <tr><td>${c.TenLop}</td><td>${c.MaKhoiLop}</td><td>${c.TenNamHoc}</td></tr>
+
+        <tr><td>${c.MaLop}</td><td>${c.TenLop}</td><td>${c.MaKhoiLop}</td><td>${c.TenNamHoc}</td></tr>
     `).join('');
 
     // Cập nhật các thẻ Select (Dropdown) trong form tạo Lớp
